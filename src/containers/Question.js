63ff990a6676844800e49b7ba1fc2da17708271a
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-09-13 17:20:45 +0200

    router with question#show

---
+++ src/containers/Question.js
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux'
+
+class Question extends Component {
+  render () {
+    const { questions } = this.props
+
+    function checkId(question){
+      return question.id == questionId
+    }
+    const { questionId } = this.props.params
+    const question = questions.find(checkId)
+    const { title, body, answers } = question
+
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p>{body}</p>
+        <h3>Answers</h3>
+        {answers.map((answer, index) => {
+          return (
+            <p><small>{answer.body}</small></p>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    questions: state.questions,
+  }
+}
+
+Question.propTypes = {
+  questions: PropTypes.array.isRequired,
+}
+
+export default connect(mapStateToProps, {})(Question)
--- src/containers/Questions.js
+++ src/containers/Questions.js
@@ -1,23 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux'
-import $ from 'jquery'
-import Question from '../components/Question'
-import getQuestions from '../actions/get-questions'
+import { Link } from 'react-router'
 
 class Questions extends Component {
-  componentDidMount() {
-    const { getQuestions } = this.props
-
-    $.get("http://localhost:4000/questions.json", function(data) {
-      getQuestions({
-        questions: data.questions
-      })
-    })
-  }
-
   renderQuestion(question) {
+    const { id, title } = question
     return(
-      <Question key={question.id} question={question}/>
+      <p key={id}>
+        <Link to={`/question/${question.id}`}>{title}</Link>
+      </p>
     )
   }
 
@@ -26,7 +17,7 @@ class Questions extends Component {
 
     return (
       <div>
-        <h2>Questions</h2>
+        <h1>Questions</h1>
         { questions.map(this.renderQuestion.bind(this)) }
       </div>
     )
@@ -36,13 +27,11 @@ class Questions extends Component {
 const mapStateToProps = (state) => {
   return {
     questions: state.questions,
-    getQuestions: state.getQuestions,
   }
 }
 
 Questions.propTypes = {
   questions: PropTypes.array.isRequired,
-  getQuestions: PropTypes.func.isRequired,
 }
 
-export default connect(mapStateToProps, { getQuestions })(Questions)
+export default connect(mapStateToProps, { })(Questions)
